48c78592dea94759ea296091cb4d76e42b87f92a
Marcello Esposito <esposito.marce@gmail.com>
2025-03-28 12:06:34 +0100

    Services, user, BU added. More fake data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,73 +16,68 @@ export class AppComponent {
 
   constructor(private modalService: NgbModal) {
     this.vms = [
-      new Vm(
-        "sflnx002844",
-        "svil",
-        true,
-        new Date(2025, 2, 30, 1, 15, 34, 323),
-        undefined
-      ),
-      new Vm(
-        "sflnx003100",
-        "svil",
-        true,
-        new Date(2025, 3, 2, 8, 15, 34, 323),
-        undefined
-      ),
-      new Vm(
-        "sflnx003101",
-        "svil",
-        true,
-        new Date(2025, 3, 2, 8, 15, 34, 323),
-        undefined
-      ),
-      new Vm(
-        "sflnx003102",
-        "svil",
-        true,
-        new Date(2025, 2, 30, 4, 0, 0, 323),
-        undefined
-      ),
-      new Vm(
-        "sflnx002343",
-        "svil",
-        false,
-        undefined,
-        new Schedule(new Date(2025, 4, 1, 9, 0, 0, 0), new Date(2025, 4, 1, 18, 45, 0, 0))
-      ),
-      new Vm(
-        "sflnx002344",
-        "svil",
-        false,
-        undefined,
-        undefined
-      ),
-      new Vm(
-        "sflnx002345",
-        "svil",
-        false,
-        undefined,
-        undefined
-      ),
-      new Vm(
-        "sflnx001872",
-        "coll",
-        true,
-        new Date(2025, 4, 10, 8, 15, 34, 323),
-        undefined
-      ),
-      new Vm(
-        "sflnx003984",
-        "coll",
-        false,
-        undefined,
-        undefined
-      ),
+      new Vm('sefalr29', 'svil', 'abaco', true, this.getShutdownDate(), undefined),
+      new Vm('sefals80', 'coll', 'abaco', true, this.getShutdownDate(), undefined),
+      new Vm('sefals81', 'coll', 'abaco', false, undefined, undefined),
+      new Vm('sefals82', 'coll', 'abaco', false, undefined, undefined),
+      new Vm('sefalt58', 'svil', 'abaco', true, this.getShutdownDate(), undefined),
+      new Vm('sefalt59', 'svil', 'abaco', false, undefined, this.getSchedule()),
+      new Vm('sflnx000461', 'svil', 'siope_plus', true, this.getShutdownDate(), undefined),
+      new Vm('sflnx000462', 'coll', 'siope_plus', true, this.getShutdownDate(), undefined),
+      new Vm('wefalf58', 'coll', 'siope_plus', true, this.getShutdownDate(), undefined),
+      new Vm('wefalf59', 'coll', 'abaco', true, this.getShutdownDate(), undefined),
+      new Vm('wefalf60', 'coll', 'siope_plus', false, undefined, this.getSchedule()),
+      new Vm('wefalf61', 'coll', 'siope_plus', true, this.getShutdownDate(), undefined),
+      new Vm('wefalf78', 'coll', 'siope_plus', true, this.getShutdownDate(), undefined),
+      new Vm('wefalf79', 'coll', 'siope_plus', false, undefined, undefined),
+      new Vm('wefalj25', 'svil', 'abaco', false, undefined, undefined),
+      new Vm('wefalj36', 'svil', 'siope_plus', false, undefined, this.getSchedule()),
+      new Vm('wefalj37', 'svil', 'siope_plus', false, undefined, undefined),
+      new Vm('wefalj39', 'svil', 'siope_plus', false, undefined, undefined),
+      new Vm('wefalj80', 'svil', 'abaco', true, this.getShutdownDate(), undefined),
+      new Vm('wflnx003427', 'svil', 'siope_plus', true, this.getShutdownDate(), undefined),
+      new Vm('wflnx004121', 'svil', 'siope_plus', true, this.getShutdownDate(), undefined),
+      new Vm('wflnx004122', 'coll', 'siope_plus', true, this.getShutdownDate(), undefined),
     ]
   }
 
-  public filteredVms(env: string): Vm[] {
-    return this.vms.filter(vm => vm.environment == env);
+  private rnd(min: number, max: number): number {
+    return Math.floor(Math.random() * max + min);
+  }
+
+  private getSchedule(): Schedule {
+    var now = new Date();
+    const y = now.getFullYear();
+    const m = now.getMonth() + 1;
+    const sd = this.rnd(4, 15);
+    const ed = sd + this.rnd(2, 4);
+    const sh = this.rnd(8, 15);
+    const eh = this.rnd(17, 23);
+    console.log(y, m, sd, ed, sh, eh);
+    return new Schedule(new Date(y, m, sd, sh, 0, 0, 0), new Date(y, m, ed, eh, 0, 0, 0))
+  }
+
+  private getShutdownDate(): Date {
+    const now = new Date();
+    const then = now.getTime() + 1000 * 60 * 60 * 24 * Math.trunc(Math.random() * 8);
+    return new Date(then)
+  }
+
+  public filteredVms(srv: string, env: string): Vm[] {
+    return this.vms
+    .filter(vm => vm.service == srv)
+    .filter(vm => vm.environment == env);
+  }
+
+  public getAllServices(env: string): string[] {
+    return this.vms
+    .filter(vm => vm.environment == env)
+    .map(vm => vm.service)
+    .filter(this.onlyUnique)
+    .sort();
+  }
+
+  private onlyUnique(value: string, index: number, array: string[]) {
+    return array.indexOf(value) === index;
   }
 }
--- src/app/classes/vm.ts
+++ src/app/classes/vm.ts
@@ -2,6 +2,7 @@ export class Vm {
     public constructor(
         public readonly name: string,
         public readonly environment: string,
+        public readonly service: string,
         public poweredOn: boolean,
         public powerOffTime?: Date,
         public schedule?: Schedule,
